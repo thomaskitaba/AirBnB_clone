0790b39a7a38be236619a4ed3a28b82c1a07bf96
thomaskitaba <thomaskitaba@gmail.com>
2023-07-17 20:34:46 +0300

    2nd dead line correction

--- models/base_model.py
+++ models/base_model.py
@@ -30,21 +30,21 @@ class BaseModel:
             models.storage.new(self)
         models.storage.save()
 
-    @property
-    def email(self):
-        return self.email
+    # @property
+    # def email(self):
+    #     return self.email
 
-    @property
-    def password(self):
-        return self.password
+    # @property
+    # def password(self):
+    #     return self.password
 
-    @property
-    def last_name(self):
-        return self.last_name
+    # @property
+    # def last_name(self):
+    #     return self.last_name
 
-    @property
-    def first_name(self):
-        return self.first_name
+    # @property
+    # def first_name(self):
+    #     return self.first_name
 
     def __str__(self):
 
