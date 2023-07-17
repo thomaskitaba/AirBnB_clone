c68cfdd323871384e87b9cbd681f239dc292b5f3
thomaskitaba <thomaskitaba@gmail.com>
2023-07-17 14:49:15 +0300

    UPdate_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
+        models.storage.save()
 
     def __str__(self):
         """ __str__ """
